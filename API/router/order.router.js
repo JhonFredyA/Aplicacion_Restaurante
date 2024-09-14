c032469cea1b70fe141d381295a6441f50953d4c
unknown <APRENDIZ@BOGDAFCGFFSP415.FormacionSena.RED>
2024-09-13 21:18:05 -0500

    Se agrego logica de endpoint para registrar ordenes

--- API/router/order.router.js
+++ API/router/order.router.js
@@ -1,9 +1,18 @@
 const { Router } = require('express')
+const MySQL = require('../Libs/mysql')
 
 const router = Router()
+const mySQL = new MySQL()
 
 router.post('/recordOrder', async (req, res) => {
     try {
+        console.log( req.body)
+        const {mesero,tipo_cliente,mesa,propina} = req.body 
+        //TODO: Generar una fecha y hora para introducir en la consulta
+        const resultado = await mySQL.executeQuery('INSERT INTO Orden (Ord_Mesa,Ord_Propina,Mes_ID,TiC_ID) VALUES (?,?,?,?)',[mesa,propina,mesero,tipo_cliente])
+        //TODO: Verificar resultado
+        console.log(resultado.affectedRows) 
+        
         
     } catch (error) {
         console.error(error);
