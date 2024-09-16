56fedbc42fa39cfbe324df79a4a697b0f7708a45
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-09-15 19:43:33 -0500

    documentación de código

--- API/router/router.js
+++ API/router/router.js
@@ -3,8 +3,10 @@ const dataRouter = require('./data.router')
 const orderRouter = require('./order.router')
 
 const router = Router()
-
+// Endpoint para ejecutar operaciones relacionadas con los datos
 router.use( '/data', dataRouter )
+
+// Endpoint para ejecutar operaciones relacionadas con las ordenes
 router.use( '/order', orderRouter )
 
 module.exports = router
