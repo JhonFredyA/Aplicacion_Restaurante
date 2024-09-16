e3fcb82f1ee67a6922d2a202a9e39f75444c7a5e
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-09-15 19:43:59 -0500

    se implementa consulta faltante para obtener datos de mesas

--- API/router/data.router.js
+++ API/router/data.router.js
@@ -1,21 +1,28 @@
 const { Router } = require('express')
 const MySQL = require('../Libs/mysql')
 
-const mySQL = new MySQL()
 const router = Router()
 
+// Obtiene los datos necesarios para el registro de ordenes
+// http://localhost:3000/data/getData
 router.get('/getData', async ( req, res ) => {
     try {
+        const mySQL = new MySQL()
+        // Ejecuta las consultas para obtener los datas de cada tabla en específico
         const meseros = await mySQL.executeQuery('SELECT * FROM Mesero')
         const tipoCliente = await mySQL.executeQuery('SELECT * FROM Tipo_Cliente')
         const productos = await mySQL.executeQuery('SELECT * FROM Producto')
-        res.json({
+        const mesas = await mySQL.executeQuery('SELECT * FROM Mesa')
+        
+        res.status(200).json({
             'meseros': meseros,
-            'tipoCliente': tipoCliente,
-            'productos': productos
+            'tiposCliente': tipoCliente,
+            'productos': productos,
+            'mesas': mesas
         })
     } catch (error) {
         console.error(error);
+        res.status(500).json({ mensaje: 'Error interno del servidor' })
     }
 })
 
