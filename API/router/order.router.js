8418f0a339c6c30b20c5c61e4e49e70b06c1bef8
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2024-09-15 19:44:19 -0500

    se agrega lógica de endpoint que registra una orden

--- API/router/order.router.js
+++ API/router/order.router.js
@@ -2,20 +2,36 @@ const { Router } = require('express')
 const MySQL = require('../Libs/mysql')
 
 const router = Router()
-const mySQL = new MySQL()
 
+// Endpoint para registrar una orden
+// http://localhost:3000/order/recordOrden
 router.post('/recordOrder', async (req, res) => {
     try {
-        console.log( req.body)
-        const {mesero,tipo_cliente,mesa,propina} = req.body 
-        //TODO: Generar una fecha y hora para introducir en la consulta
-        const resultado = await mySQL.executeQuery('INSERT INTO Orden (Ord_Mesa,Ord_Propina,Mes_ID,TiC_ID) VALUES (?,?,?,?)',[mesa,propina,mesero,tipo_cliente])
-        //TODO: Verificar resultado
-        console.log(resultado.affectedRows) 
+        const mySQL = new MySQL()
+        const { mesero, tipoCliente, mesa, productos } = req.body
+        // Guarda fecha y hora actual del registro
+        const fecha = new Date() 
+        const propina = 0.13
+        // Convierte los datos recibidos en productos en un Array
+        const productosArray = productos.split(',').map(id => id.trim()).filter(id => id.length > 0)
+        // Ejecuta sentencia SQL para insertar datos de la orden
+        const resultadoOrden = await mySQL.executeQuery('INSERT INTO Orden(Ord_Fecha,Ord_Propina,Mesa_ID,Mes_ID,TiC_ID) VALUES (?,?,?,?,?)',[fecha, propina, mesa, mesero, tipoCliente])
+        // Obtiene el ID de la orden recién creada
+        const ordenID = resultadoOrden.insertId
+        // Crea un Array que contiene los pares de valores para cada producto
+        const valoresPedido = productosArray.map(productoId => [ordenID, productoId])
+        // Ejecuta la sentencia SQL para insetar los datos del pedido
+        const resultadoPedido = await mySQL.executeQuery('INSERT INTO Pedido(Ord_ID, Prod_ID) VALUES ?', [valoresPedido])        
         
-        
+        // Verifica si los datos de ambas consultas se insertaron correctamente
+        if (resultadoOrden.affectedRows > 0 && resultadoPedido.affectedRows > 0) {
+            res.status(200).json({ mensaje: 'Registro de orden exitoso' })
+        } else {
+            res.status(500).json({ mensaje: 'Error al registrar la orden o pedido' })
+        }
     } catch (error) {
-        console.error(error);
+        console.error(error)
+        res.status(500).json({ mensaje: 'Error interno del servidor' })
     }
 })
 
